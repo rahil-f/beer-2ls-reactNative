542a12cd21b992d3b8698967b87c3318ecf3b06a
Felix RAHIL <felixrahil@yahoo.fr>
2022-06-13 18:06:27 +0200

    add separator and index

---
+++ components/Separator.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+interface SeparatorProps {
+
+};
+
+export const Separator: React.FC<SeparatorProps> = ({}) => {
+  return (
+    <View/>
+  );
+};
+
